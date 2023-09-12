e19886db5f3e3401f7b4bb0b8c733fd6f484fd47
Ian Cinti <ian.cinti@gmail.com>
2023-09-12 05:19:32 -0300

    Impl boton guardar nuevo cliente

--- src/main/java/clienteapp/springbootclienteapp/controller/ClienteController.java
+++ src/main/java/clienteapp/springbootclienteapp/controller/ClienteController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
@@ -40,6 +42,13 @@ public class ClienteController {
         model.addAttribute("cliente", cliente);
         model.addAttribute("ciudades", listCiudades);
         return "/views/clientes/frmCrear";
+    }
+
+    @PostMapping("/save")
+    public String guardar(@ModelAttribute Cliente cliente) {
+
+        clienteService.guardar(cliente);
+        return "redirect:/views/clientes/";
     }
 }
 
