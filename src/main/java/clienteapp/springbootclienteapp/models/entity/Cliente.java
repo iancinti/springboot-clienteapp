83c3eb542b153cb64520a3c766f25dd13264fb4a
Ian Cinti <ian.cinti@gmail.com>
2023-09-12 01:22:13 -0300

    Creacion clase Cliente

--- src/main/java/clienteapp/springbootclienteapp/models/entity/Cliente.java
+++ src/main/java/clienteapp/springbootclienteapp/models/entity/Cliente.java
@@ -1,4 +1,82 @@
 package clienteapp.springbootclienteapp.models.entity;
 
-public class Cliente {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private String telefono;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name="ciudad")
+    private Ciudad ciudad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                ", ciudad=" + ciudad +
+                '}';
+    }
 }
