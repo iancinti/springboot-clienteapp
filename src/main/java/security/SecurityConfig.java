f6d06bbd2bb65efc1712889a9f5f67316fe4fc00
Ian Cinti <ian.cinti@gmail.com>
2023-10-04 22:06:04 -0300

    impl login Spring Security

--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -1,10 +1,11 @@
 package security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 
 @Configuration
 @EnableWebSecurity
@@ -29,5 +30,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("ian")
+                .password("{noop}miclave")
+                .roles("BASICO");
     }
 }
