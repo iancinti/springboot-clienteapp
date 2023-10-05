e8948b969bfde12427545ff7772901304156646d
Ian Cinti <ian.cinti@gmail.com>
2023-10-05 00:07:42 -0300

    creation entity Usuario

---
+++ src/main/java/clienteapp/springbootclienteapp/models/entity/Usuario.java
@@ -0,0 +1,25 @@
+package clienteapp.springbootclienteapp.models.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
